1b830a0a961ff1232ae3ab7e093cae0e594b3649
Minh Vu <minhd_vu@yahoo.com>
2021-10-12 19:53:17 -0400

    added keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-use bevy::prelude::*;
+use bevy::{
+    input::{keyboard::KeyCode, Input},
+    prelude::*,
+};
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_system(keyboard_input_system.system())
+        .run();
+}
+
+/// This system prints keyboard key state
+fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up) {
+        println!("up just pressed");
+    }
+
+    if keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down) {
+        println!("down just pressed");
+    }
+
+    if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left) {
+        println!("left just pressed");
+    }
+
+    if keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right) {
+        println!("right just pressed");
+    }
 }
