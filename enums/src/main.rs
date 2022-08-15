c6f6c2269e43988cc2ef0340a9d3e550260c32d3
Minh Vu <minhd_vu@yahoo.com>
2022-08-15 14:36:22 -0400

    added match and if let

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -25,4 +25,15 @@ fn main() {
     // Some represents that there is some value of type T present in the enum, and None represents
     // something similar to null. Generally this allows us to catch something when something is 
     // expected to not be null, when it actaully is.
+
+    // In the match pattern, it will match the first one, going downwards. Matches are exhaustive
+    // which means the arms of a match must include all possible paths. You can use _ as a catch
+    // all pattern for match. You can also use the unit in order to have something do nothing.
+    
+    // When the match control flow is too wordy, we can use the if let syntax.
+    let v = Some(100);
+    // Here we are creating a new varaible u which has the value of v if it is in the Some arm.
+    if let Some(u) = v {
+        println!("{}", u);
+    }
 }
