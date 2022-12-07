920e9619c04995d65d041841184858f5e3bb9d85
Minh Vu <minhd_vu@yahoo.com>
2022-12-07 01:51:59 -0500

    add backtrace comment

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    panic!("crash and burn");
+    // Manually force a unrecoverable error
+    // panic!("crash and burn");
+
+    // Try to access an index out of bounds.
+    // To see the backtrace, run:
+    // RUST_BACKTRACE=1 cargo run
+    let v = vec![1, 2, 3];
+    v[99];
 }
