1abfac1536ededd40c7e1505cb28b4786539ff4d
minhd-vu <minhd_vu@yahoo.com>
2023-02-03 00:51:19 -0500

    add search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,8 +24,43 @@ impl Config {
 // return any type that implements Error.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}")
+    }
 
     // This is the idiomatic way of calling run only for its side effects.
     Ok(())
+}
+
+// Here the lifetimes indicate that the vector should return string slices that
+// reference the contents argument rather than the query argument i.e. the data
+// returned by search will live as long as the data in the argument contents
+// (not query!)
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        // The backslash at the start tells rust to not put a newline at the start.
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
