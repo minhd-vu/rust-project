0c3c95dd9235d230f0fb53f1aedaea1a2a81581a
minhd-vu <minhd_vu@yahoo.com>
2023-05-14 01:51:02 -0400

    simulate long request

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -32,10 +34,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = &http_request[0];
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     // Read in the HTML file and return it as the response.
