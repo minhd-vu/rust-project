9de8155c0a4894254490d66f9287d5d8e9ee92e8
minhd-vu <minhd_vu@yahoo.com>
2023-05-15 02:28:02 -0400

    add new and build methods

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,8 +1,30 @@
 pub struct ThreadPool;
+pub struct PoolCreationError;
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
+    }
+
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// The `build` function will return an error if the size is zero.
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size > 0 {
+            return Ok(ThreadPool);
+        }
+        Err(PoolCreationError)
     }
 
     pub fn execute<F>(&self, f: F)
