c1b8dc0e58cf0c973921fefe468e9473847b998c
minhd-vu <minhd_vu@yahoo.com>
2024-01-25 15:13:09 -0500

    add dynamic attributes

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -9,6 +9,42 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let (x, set_x) = create_signal(0);
 
-    view! { <button on:click=move |_| { set_count.update(|n| *n += 1) }>"Click me: " {count}</button> }
+    // derived signals
+    let double_count = move || count() * 2;
+    let html = "<p>This HTML will be injected.</p>";
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+                set_x.update(|n| *n += 10);
+            }
+
+            class:red=move || count() % 2 == 1
+            // set the `style` attribute
+            style="position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style:left=move || format!("{}px", x() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", x)
+        >
+            "Click me: "
+            {count}
+        </button>
+        <progress
+            max="50"
+            // we use it once here
+            value=double_count
+        ></progress>
+        <p>
+            // and again here
+            "Double Count: " {double_count}
+        </p>
+        // injected html
+        <div inner_html=html></div>
+    }
 }
